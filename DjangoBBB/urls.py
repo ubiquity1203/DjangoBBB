86d3444a3a3d73594509528ef7136da640ccdf14
Tobia <tobia.ghiraldini@ninjabit.com>
2013-09-18 18:42:50 +0200

    Homepage url

--- DjangoBBB/urls.py
+++ DjangoBBB/urls.py
@@ -5,22 +5,27 @@ from django.conf.urls.i18n import i18n_patterns
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django.http.response import HttpResponseNotFound, HttpResponseServerError
+from django.views.generic.base import TemplateView
 
 admin.autodiscover()
 
 # ADMIN
 urlpatterns = patterns('',
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
 )
 
 # AUTH
 urlpatterns += i18n_patterns('',
-    # Registration urls
-    url(r'^accounts/', include('registration.backends.default.urls')),
+                             # Registration urls
+                             url(r'^accounts/', include('registration.backends.default.urls')),
+)
+
+# HOME
+urlpatterns += i18n_patterns('',
+                             url(r'^$', TemplateView.as_view(template_name='index.html'), name='index')
 )
 
 
@@ -32,20 +37,20 @@ urlpatterns += i18n_patterns('',
 ## Served from nginx proxy in test and prod
 if settings.DEBUG:
     urlpatterns = patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-        url(r'', include('django.contrib.staticfiles.urls')),
+                           url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+                           url(r'', include('django.contrib.staticfiles.urls')),
     ) + urlpatterns
 
 ## Rosetta urls
 if settings.DEBUG:
     if 'rosetta' in settings.INSTALLED_APPS:
         urlpatterns += patterns('',
-            url(r'^rosetta/', include('rosetta.urls')),
+                                url(r'^rosetta/', include('rosetta.urls')),
         )
 
 ## Error page test urls
 if settings.DEBUG:
     urlpatterns = patterns('',
-        url(r'^test404/', HttpResponseNotFound),
-        url(r'^test500/', HttpResponseServerError),
+                           url(r'^test404/', HttpResponseNotFound),
+                           url(r'^test500/', HttpResponseServerError),
 ) + urlpatterns
