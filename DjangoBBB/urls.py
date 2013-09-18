c2dc13bb148d51f84612db4ac8e00b4d8e588c3b
Tobia <tobia.ghiraldini@ninjabit.com>
2013-09-18 18:18:52 +0200

    Project urls configured

---
+++ DjangoBBB/urls.py
@@ -0,0 +1,51 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from django.http.response import HttpResponseNotFound, HttpResponseServerError
+
+admin.autodiscover()
+
+# ADMIN
+urlpatterns = patterns('',
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+# AUTH
+urlpatterns += i18n_patterns('',
+    # Registration urls
+    url(r'^accounts/', include('registration.backends.default.urls')),
+)
+
+
+#########################################
+#                                       #
+#                DEBUG                  #
+#                                       #
+#########################################
+## Served from nginx proxy in test and prod
+if settings.DEBUG:
+    urlpatterns = patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        url(r'', include('django.contrib.staticfiles.urls')),
+    ) + urlpatterns
+
+## Rosetta urls
+if settings.DEBUG:
+    if 'rosetta' in settings.INSTALLED_APPS:
+        urlpatterns += patterns('',
+            url(r'^rosetta/', include('rosetta.urls')),
+        )
+
+## Error page test urls
+if settings.DEBUG:
+    urlpatterns = patterns('',
+        url(r'^test404/', HttpResponseNotFound),
+        url(r'^test500/', HttpResponseServerError),
+) + urlpatterns
