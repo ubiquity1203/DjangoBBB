9938dba2ed3dd5c61f1ef0b9b314a1a1c71fa664
Tobia <tobia.ghiraldini@ninjabit.com>
2013-09-25 18:46:33 +0200

    split settings refactor

--- DjangoBBB/settings/components/base.py
+++ DjangoBBB/settings/components/base.py
@@ -15,8 +15,9 @@ ENV = 'Development'
 
 gettext = lambda s: s
 
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+ROOT_PATH = os.getcwd()
 PROJECT_NAME = os.getcwd().split(os.sep)[-1]
+PROJECT_PATH = os.path.join(ROOT_PATH,PROJECT_NAME)
 
 
 #############################
@@ -34,7 +35,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_PATH, '../../database.sqlite'), # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_PATH, 'database.sqlite'), # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -93,27 +94,27 @@ LOCALE_PATHS = (
 #############################
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, "../../media")
+MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, "../../static_root")
+STATIC_ROOT = os.path.join(PROJECT_PATH, "static_root")
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, "../../static"),
+    os.path.join(PROJECT_PATH, "static"),
 )
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, "../../templates"),
+    os.path.join(PROJECT_PATH, "templates"),
 )
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
@@ -259,35 +260,3 @@ INSTALLED_APPS = (
     'api',
     'utilities',
 )
-
-
-#############################
-#          CONFS            #
-#############################
-# To keep settings minimal and usable
-# other settings file are placed into conf/ path as .conf files
-# Loading other settings
-settingsfiles = glob.glob(os.path.join(os.path.dirname(__file__), '../../conf', '*.conf.py'))
-settingsfiles.sort()
-for f in settingsfiles:
-    try:
-        execfile(os.path.abspath(f))
-    except NameError:
-        print('Exception executing file: %s'% f)
-
-# Loading local settings
-try:
-    from DjangoBBB.settings.local_settings import *
-except ImportError:
-    pass
-# Loading test/prod settings based on ENV settings
-if ENV == 'test':
-    try:
-        from DjangoBBB.settings.test_settings import *
-    except ImportError:
-        pass
-elif ENV == 'prod':
-    try:
-        from DjangoBBB.settings.prod_settings import *
-    except ImportError:
-        pass
